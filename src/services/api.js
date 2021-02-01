c00fbb7628840e3dde7bede64da51645a5aea544
johnnyarza <johnnyarza@gmail.com>
2021-02-01 00:00:56 -0300

    removed dotenv

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import 'dotenv/config';
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.BASE_URL || 'http://192.168.1.9:3333',
 });
+console.log(process.env.BASE_URL);
 
 export default api;
