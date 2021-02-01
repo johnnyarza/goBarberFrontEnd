140260c22feaad53af20924e0d1f5600c5478bb4
johnnyarza <johnnyarza@gmail.com>
2021-02-01 00:14:17 -0300

    changed api congfig

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || 'http://192.168.1.9:3333',
+  baseURL: process.env.BASE_URL || 'http://192.81.219.9:3333',
 });
-console.log(process.env.BASE_URL);
 
 export default api;
