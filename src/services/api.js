cda8108a0288055936b694e1ed826176c8444406
johnnyarza <johnnyarza@gmail.com>
2021-01-31 23:36:13 -0300

    Changed api config

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import 'dotenv/config';
 import axios from 'axios';
 
-const api = axios.create({ baseURL: 'http://192.168.1.9:3333' });
+const api = axios.create({
+  baseURL: process.env.BASE_URL || 'http://192.168.1.9:3333',
+});
 
 export default api;
