1c7a8d8eda6f7d2119aff523c36654cf438b16aa
johnnyarza <johnnyarza@gmail.com>
2021-02-01 01:13:07 -0300

    changed api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.BASE_URL || 'http://192.81.219.9:3333',
+  baseURL: process.env.BASE_URL || 'http://192.81.219.9',
 });
 
 export default api;
